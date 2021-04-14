7e0f4898ff2a0cb067bc0c4f36ea25980932d7ff
MattTJZW <74039180+MattTJZW@users.noreply.github.com>
2021-04-14 12:43:52 +0200

    [ADD] app deltatech_price_categ

---
+++ deltatech_price_categ/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Price List: Bronze Silver and Gold ",
+    "version": "12.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": ["product", "account", "sale"],
+    "license": "AGPL-3",
+    "data": ["views/product_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+}
